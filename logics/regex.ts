7ac08af81ca2d6ae5884fbb3aa0a6523e3fa0b44
hmmhmmhm <hmmhmmhm@naver.com>
2020-03-15 20:39:56 +0900

    Add Regex

--- logics/regex.ts
+++ logics/regex.ts
@@ -1,4 +1,10 @@
 const emailRegex = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i
+
+const numberWithCommas = x => {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+}
+
 export const Regex = {
     emailRegex,
+    numberWithCommas,
 }
